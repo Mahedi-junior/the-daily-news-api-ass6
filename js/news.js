6ca6014f319dbd130baf47c34e1774998167342c
Mahedi hasan <mahedi2112p@gmail.com>
2022-10-17 15:03:05 +0600

    news categories added

--- js/news.js
+++ js/news.js
@@ -9,7 +9,7 @@ const displayCategory = categories => {
     // console.log(categories);
     const categoriesContainer = document.getElementById("categories-container");
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const ulDiv = document.createElement("ul");
         ulDiv.classList.add("navbar-nav");
         ulDiv.innerHTML = `
@@ -26,11 +26,33 @@ const loadNews = async (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayNews(data.data[0]);
+    displayNews(data.data);
 }
 
-const displayNews = (news) => {
-    console.log(news);
+const displayNews = (allNews) => {
+    const newsContainer = document.getElementById("news-container");
+    newsContainer.innerHTML = '';
+
+    for (const news of allNews) {
+        console.log(news);
+        const newsDiv = document.createElement("div");
+        newsDiv.classList.add("row");
+        newsDiv.innerHTML = `
+        <div class="col-md-4">
+            <img src=${news.image_url} class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+           <div class="card-body">
+               <h5 class="card-title">${news.title}</h5>
+               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.p>
+               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        
+        `;
+        newsContainer.appendChild(newsDiv);
+
+    }
 }
 
 // loadNews();
